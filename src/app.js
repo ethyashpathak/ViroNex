59da8bcbd238e936a6bcc75dcf75a5e31fafbd7b
ethyashpathak <ethyashpathak@gmail.com>
2025-08-29 22:51:33 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -13,4 +13,10 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//route import
+import { userRouter } from './routes/user.routes.js';
+
+//route declaration
+app.use("/api/v1/users",userRouter)
+
 export {app}
