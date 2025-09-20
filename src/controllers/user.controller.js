96655663e55a8ca70931685fe11b39ed62b8532d
ethyashpathak <ethyashpathak@gmail.com>
2025-09-20 14:53:43 +0530

    updated user controller and made auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -158,4 +158,28 @@ const loginUser = asynchandler(async (req, res) => {
 
 });
 
+const logoutUser=asynchandler(async(req,res)=>{
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set:{
+        refreshToken:undefined
+      }
+    },
+    {
+      new:true
+    }
+  )
+  const options={
+    httOnly:true,
+    secure:true
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken",options)
+  .clearCookie("refreshToken",options)
+  .json(new ApiResponse(200,{},"User logged out"))
+})
+
 export { registerUser };
