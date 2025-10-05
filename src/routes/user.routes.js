adea6eeda556bd20e349a48d136c347e9a1baeac
ethyashpathak <ethyashpathak@gmail.com>
2025-10-05 18:51:51 +0530

    Update user routes to improve functionality and fix issues

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser
+  ,loginUser
+  ,logoutUser
+  ,refreshAccessToken
+  , changeCurrentPassword
+  , getCurrentUser
+  , updateAccountDetails
+  , updateUserAvatar
+  , updateUserCoverImage
+  , getUserChannelProfile
+  , getUserHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -20,13 +30,15 @@ userRouter.route("/register").post(
     registerUser
 
 );
-
-
-
 userRouter.route("/login").post(loginUser)
-
 userRouter.route("/logout").post(verifyJWT,  logoutUser)
 userRouter.route("/refresh-token").post(refreshAccessToken)
-
+userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+userRouter.route("/update-details").patch(verifyJWT, updateAccountDetails)
+userRouter.route("/change-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+userRouter.route("/change-cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+userRouter.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+userRouter.route("/watch-history").get(verifyJWT, getUserHistory)
 
 export default userRouter
